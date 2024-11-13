5e32a44b5143a1de5c8f0ba8ad942a0230c1e0f3
iamnana-tyc <nanatyc7@gmail.com>
2024-11-13 17:21:09 +0500

    implement mapper for order line

---
+++ services/order/src/main/java/com/iamnana/microservice/orderLine/OrderLineMapper.java
@@ -0,0 +1,26 @@
+package com.iamnana.microservice.orderLine;
+
+import com.iamnana.microservice.order.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderLineMapper{
+    public OrderLine toOrderLine(OrderLineRequest request) {
+        return OrderLine.builder()
+                .id(request.id())
+                .quantity(request.quantity())
+                .order(Order.builder()
+                        .id(request.orderId())
+                        .build()
+                )
+                .productId(request.productId())
+                .build();
+    }
+
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return new OrderLineResponse(
+                orderLine.getId(),
+                orderLine.getQuantity()
+        );
+    }
+}
