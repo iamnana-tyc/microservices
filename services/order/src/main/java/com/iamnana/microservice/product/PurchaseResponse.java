34f185d384b2f2cbbfa9516900f43c9215e73613
iamnana-tyc <nanatyc7@gmail.com>
2024-11-13 17:16:29 +0500

    add record for purchase response

---
+++ services/order/src/main/java/com/iamnana/microservice/product/PurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.iamnana.microservice.product;
+
+import java.math.BigDecimal;
+
+public record PurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
