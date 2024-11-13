e7e78048353ca9da0d6a321e467268b309130a27
iamnana-tyc <nanatyc7@gmail.com>
2024-11-13 16:58:38 +0500

    add a product response

---
+++ services/product/src/main/java/com/iamnana/microservice/product/ProductPurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.iamnana.microservice.product;
+
+import java.math.BigDecimal;
+
+public record ProductPurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
