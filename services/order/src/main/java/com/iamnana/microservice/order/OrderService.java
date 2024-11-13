d85b2680c290788db6b3f5cfa81b1cc6288083ae
iamnana-tyc <nanatyc7@gmail.com>
2024-11-13 17:07:39 +0500

    implement order service

---
+++ services/order/src/main/java/com/iamnana/microservice/order/OrderService.java
@@ -0,0 +1,79 @@
+package com.iamnana.microservice.order;
+
+import com.iamnana.microservice.customer.CustomerClient;
+import com.iamnana.microservice.exception.BusinessException;
+import com.iamnana.microservice.kafka.OrderConfirmation;
+import com.iamnana.microservice.kafka.OrderProducer;
+import com.iamnana.microservice.orderLine.OrderLineRequest;
+import com.iamnana.microservice.orderLine.OrderLineService;
+import com.iamnana.microservice.product.ProductClient;
+import com.iamnana.microservice.product.PurchaseRequest;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderMapper mapper;
+    private final OrderRepository repository;
+    private final CustomerClient customerClient;
+    private final ProductClient productClient;
+    private final OrderLineService orderLineService;
+    private final OrderProducer orderProducer;
+
+    public Integer createOrder(OrderRequest request) {
+        // we need to check client exist using open feign
+        var customer = this.customerClient.findCustomerById(request.customerId())
+                .orElseThrow(()-> new BusinessException("Can't create order:: No customer exist with the provided "));
+
+        // we need to purchase the product using RestTemplate
+        var purchasedProducts = this.productClient.purchaseProducts(request.products());
+
+        // now we have to persist the order
+        var order = repository.save(mapper.toOrder(request));
+
+        // we need to persist the order lines
+        for(PurchaseRequest purchaseRequest : request.products()){
+            orderLineService.saveOrderLine(
+                    new OrderLineRequest(
+                            null,
+                            order.getId(),
+                            purchaseRequest.productId(),
+                            purchaseRequest.quantity()
+                    )
+            );
+        }
+
+        // TODO we need to start the payment process
+
+        // send order confirmation --> notification sms using Kafka
+        orderProducer.sendOrderConfirmation(
+                new OrderConfirmation(
+                        request.reference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        customer,
+                        purchasedProducts
+                )
+        );
+
+        return order.getId();
+    }
+
+    public List<OrderResponse> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::fromOrder)
+                .collect(Collectors.toList());
+    }
+
+    public OrderResponse findById(Integer orderId) {
+        return repository.findById(orderId)
+                .map(mapper::fromOrder)
+                .orElseThrow(()-> new EntityNotFoundException(String.format("No order found with the provided ID: %d", orderId)));
+    }
+}
