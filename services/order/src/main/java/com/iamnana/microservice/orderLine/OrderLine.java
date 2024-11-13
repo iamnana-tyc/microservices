481513a8307751d3800caed525e48d36aaff7372
iamnana-tyc <nanatyc7@gmail.com>
2024-11-13 17:20:10 +0500

    class for order line

---
+++ services/order/src/main/java/com/iamnana/microservice/orderLine/OrderLine.java
@@ -0,0 +1,23 @@
+package com.iamnana.microservice.orderLine;
+
+import com.iamnana.microservice.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private Integer productId;
+    private double quantity;
+}
