94a9b3ca24196584cf3923b3220c6d85a6d52925
iamnana-tyc <nanatyc7@gmail.com>
2024-11-18 12:25:31 +0500

    implement notification class

---
+++ services/notification/src/main/java/com/iamnana/microservice/notification/Notification.java
@@ -0,0 +1,25 @@
+package com.iamnana.microservice.notification;
+
+
+import com.iamnana.microservice.kafka.order.OrderConfirmation;
+import com.iamnana.microservice.kafka.payment.PaymentConfirmation;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
