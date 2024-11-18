739d1f29c2bc878310d1889f060a3ff3867ff4b7
iamnana-tyc <nanatyc7@gmail.com>
2024-11-18 12:07:49 +0500

    add payment service

---
+++ services/payment/src/main/java/com/iamnana/microservice/payment/PaymentService.java
@@ -0,0 +1,32 @@
+package com.iamnana.microservice.payment;
+
+
+import com.iamnana.microservice.notification.NotificationProducer;
+import com.iamnana.microservice.notification.PaymentNotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest request) {
+        var payment = repository.save(mapper.toPayment(request));
+
+        // send notification
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstName(),
+                        request.customer().lastName(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
+    }
+}
