352184831205d3b6d5e95b28ca6d26a731336a23
iamnana-tyc <nanatyc7@gmail.com>
2024-11-18 12:34:53 +0500

    add Payment Repository

---
+++ services/payment/src/main/java/com/iamnana/microservice/payment/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.iamnana.microservice.payment;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment,Integer> {
+}
