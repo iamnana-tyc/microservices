add7944a426c1db31cb5e1dea93255242d6dff13
iamnana-tyc <nanatyc7@gmail.com>
2024-11-18 12:32:39 +0500

    add payment notification request

---
+++ services/payment/src/main/java/com/iamnana/microservice/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.iamnana.microservice.notification;
+
+import com.iamnana.microservice.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
