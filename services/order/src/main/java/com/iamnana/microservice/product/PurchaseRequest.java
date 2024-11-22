06d5d46d32220cdf73866b6d8bfe0a73ae5da348
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 15:06:53 +0500

    add validated to the PurchaseRequest class

--- services/order/src/main/java/com/iamnana/microservice/product/PurchaseRequest.java
+++ services/order/src/main/java/com/iamnana/microservice/product/PurchaseRequest.java
@@ -2,7 +2,9 @@ package com.iamnana.microservice.product;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public record PurchaseRequest(
         @NotNull(message = "Product is mandatory")
         Integer productId,
