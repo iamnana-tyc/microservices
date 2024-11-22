448b13b20cb35bde0d220ef88d530dc52d7e6c8e
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 15:02:36 +0500

    correct the order controller request mapping

--- services/order/src/main/java/com/iamnana/microservice/order/OrderController.java
+++ services/order/src/main/java/com/iamnana/microservice/order/OrderController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/orders")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService service;
