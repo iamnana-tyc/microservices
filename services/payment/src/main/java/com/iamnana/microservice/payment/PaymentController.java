bec448fbbe737fdbb750e03017a422acada1c1a4
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 15:08:32 +0500

    correct mistake in Payment controller request mapping

--- services/payment/src/main/java/com/iamnana/microservice/payment/PaymentController.java
+++ services/payment/src/main/java/com/iamnana/microservice/payment/PaymentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/payments")
+@RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService service;
