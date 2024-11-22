64f28606d7bea5992b8cc480ac8b325290ef2111
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 14:59:34 +0500

    add notification repository

--- services/notification/src/main/java/com/iamnana/microservice/notification/NotificationRepository.java
+++ services/notification/src/main/java/com/iamnana/microservice/notification/NotificationRepository.java
@@ -1,5 +1,6 @@
 package com.iamnana.microservice.notification;
 
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface NotificationRepository extends MongoRepository<Notification, String> {
