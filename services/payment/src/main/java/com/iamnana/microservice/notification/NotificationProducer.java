cf03c98fde1dafb966850d8003a8d5cad89083fd
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 15:11:28 +0500

    add notification producer

--- services/payment/src/main/java/com/iamnana/microservice/notification/NotificationProducer.java
+++ services/payment/src/main/java/com/iamnana/microservice/notification/NotificationProducer.java
@@ -8,6 +8,8 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.kafka.support.KafkaHeaders.TOPIC;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -15,10 +17,10 @@ public class NotificationProducer {
     private final KafkaTemplate<String, PaymentNotificationRequest> kafkaTemplate;
 
     public void sendNotification(PaymentNotificationRequest request){
-        log.info("Sending notification with body <{}>", request);
+        log.info("Sending notification with body = < {} >", request);
         Message<PaymentNotificationRequest> message = MessageBuilder
                 .withPayload(request)
-                .setHeader(KafkaHeaders.TOPIC, "payment-topic")
+                .setHeader(TOPIC, "payment-topic")
                 .build();
         kafkaTemplate.send(message);
     }
