ab32a05da5176e3aad24ef76f3f740f2e0271746
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 15:00:13 +0500

    correct package name

--- services/order/src/main/java/com/iamnana/microservice/configuration/RestTemplateConfig.java
+++ services/order/src/main/java/com/iamnana/microservice/configuration/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.iamnana.microservice.config;
+package com.iamnana.microservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
