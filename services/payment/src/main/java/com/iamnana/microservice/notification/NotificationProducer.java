995830ec7e866557595864bdbdf4f84f5ea7c6b9
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 15:12:20 +0500

    remove unused imports

--- services/payment/src/main/java/com/iamnana/microservice/notification/NotificationProducer.java
+++ services/payment/src/main/java/com/iamnana/microservice/notification/NotificationProducer.java
@@ -3,7 +3,6 @@ package com.iamnana.microservice.notification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
