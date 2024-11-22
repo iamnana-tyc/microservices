a229e657c6937fead636f1d5cb7c8d59d916f9f0
iamnana-tyc <nanatyc7@gmail.com>
2024-11-22 14:59:56 +0500

    correct package name

--- services/order/src/main/java/com/iamnana/microservice/configuration/KafkaOrderTopicConfig.java
+++ services/order/src/main/java/com/iamnana/microservice/configuration/KafkaOrderTopicConfig.java
@@ -1,4 +1,4 @@
-package com.iamnana.microservice.config;
+package com.iamnana.microservice.configuration;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
